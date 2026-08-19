a330d6e073b204853d0f757b5fd7c3847e44ae94
Walyson Scarazzati da Silva <walyson.scarazzati@gmail.com>
2026-08-19 12:14:02 +0100

    16. Finalizando a implementacao do microservico de produtos

---
+++ produtos/src/main/java/io/github/cursodsousa/icompras/produtos/controller/ProdutoController.java
@@ -0,0 +1,28 @@
+package io.github.cursodsousa.icompras.produtos.controller;
+
+import io.github.cursodsousa.icompras.produtos.model.Produto;
+import io.github.cursodsousa.icompras.produtos.service.ProdutoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("produtos")
+@RequiredArgsConstructor
+public class ProdutoController {
+    private final ProdutoService service;
+
+    @PostMapping
+    public ResponseEntity<Produto> salvar(@RequestBody Produto produto){
+        service.salvar(produto);
+        return ResponseEntity.ok(produto);
+    }
+
+    @GetMapping("{codigo}")
+    public ResponseEntity<Produto> obterDados(@PathVariable("codigo") Long codigo){
+        return  service.obterPorCodigo(codigo)
+                .map(ResponseEntity::ok)
+                .orElseGet(()->ResponseEntity.notFound().build());
+    }
+}
+

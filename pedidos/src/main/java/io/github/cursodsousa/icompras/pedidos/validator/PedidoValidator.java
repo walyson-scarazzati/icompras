e35cf18026f7d216ba554f1956ac8f17bb153c76
Walyson Scarazzati da Silva <walyson.scarazzati@gmail.com>
2026-08-26 08:40:17 +0100

    30. Comunicação entre os microservices para validação

--- pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/validator/PedidoValidator.java
+++ pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/validator/PedidoValidator.java
@@ -1,21 +1,48 @@
 package io.github.cursodsousa.icompras.pedidos.validator;
 
+import feign.FeignException;
 import io.github.cursodsousa.icompras.pedidos.client.ClientesClient;
 import io.github.cursodsousa.icompras.pedidos.client.ProdutosClient;
+import io.github.cursodsousa.icompras.pedidos.client.representation.ClienteRepresentation;
+import io.github.cursodsousa.icompras.pedidos.client.representation.ProdutoRepresentation;
+import io.github.cursodsousa.icompras.pedidos.model.ItemPedido;
 import io.github.cursodsousa.icompras.pedidos.model.Pedido;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class PedidoValidator {
 
     private final ProdutosClient produtosClient;
     private final ClientesClient clientesClient;
 
     public void validar(Pedido pedido){
+        Long codigoCliente = pedido.getCodigoCliente();
+        validarCliente(codigoCliente);
+        pedido.getItens().forEach(this::validarItem);
+    }
 
+    private void validarCliente(Long codigoCliente){
+        try{
+            var response = clientesClient.obterDados(codigoCliente);
+            ClienteRepresentation cliente = response.getBody();
+            log.info("Cliente de codigo {} encontrado: {}", cliente.codigo(), cliente.nome());
+        } catch (FeignException.NotFound ex){
+        	log.error("Cliente não encontrado");
+        }
+    }
 
+   private void validarItem(ItemPedido item){
+        try {
+            var response = produtosClient.obterDados(item.getCodigoProduto());
+            ProdutoRepresentation produto = response.getBody();
+            log.info("Produto de código {} encontrado: {}", produto.codigo(), produto.nome());
+        } catch (FeignException.NotFound e){
+        	log.error("Produto não encontrado");
+        }
     }
 }

e3c1f0860625523d8a8688ce7bf3bf13faf0f346
Walyson Scarazzati da Silva <walyson.scarazzati@gmail.com>
2026-08-27 12:24:51 +0100

    41. Conectando no Kafka e publicando mensagem de teste

--- pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/PedidosApplication.java
+++ pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/PedidosApplication.java
@@ -1,10 +1,18 @@
 package io.github.cursodsousa.icompras.pedidos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class PedidosApplication {
+
+/*	@Bean
+	public CommandLineRunner commandLineRunner(KafkaTemplate<String,String> template){
+		return args ->template.send("icompras.pedidos-pagos", "dados", "{json}");
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);

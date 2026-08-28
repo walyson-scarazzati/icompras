0ee89886712e3688baa5f5f1ce594d3e5c87bbad
Walyson Scarazzati da Silva <walyson.scarazzati@gmail.com>
2026-08-28 11:56:05 +0100

    50. Fazendo upload para o Bucket através da API

--- faturamento/src/main/java/io/github/cursodsousa/icompras/faturamento/bucket/BucketService.java
+++ faturamento/src/main/java/io/github/cursodsousa/icompras/faturamento/bucket/BucketService.java
@@ -1,9 +1,14 @@
 package io.github.cursodsousa.icompras.faturamento.bucket;
 
 import io.github.cursodsousa.icompras.faturamento.config.props.MinioProps;
+import io.minio.GetPresignedObjectUrlArgs;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.http.Method;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -12,9 +17,30 @@ public class BucketService {
     private final MinioProps props;
 
     public void upload(BucketFile file){
-
+        try{
+            var object = PutObjectArgs.builder()
+                    .bucket(props.getBucketName())
+                    .object(file.name())
+                    .stream(file.is(), file.size(), -1)
+                    .contentType(file.type().toString())
+                    .build();
+            client.putObject(object);
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
     }
     public String getUrl(String fileName){
+        try {
+            var object = GetPresignedObjectUrlArgs.builder()
+                    .method(Method.GET)
+                    .bucket(props.getBucketName())
+                    .object(fileName)
+                    .expiry(1, TimeUnit.HOURS)
+                    .build();
 
+           return client.getPresignedObjectUrl(object);
+        } catch (Exception e){
+            throw  new RuntimeException(e);
+        }
     }
 }

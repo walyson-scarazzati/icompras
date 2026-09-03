2fe4d77dbd890eadf4ab5c0b649e1ca6758f0afe
Walyson Scarazzati da Silva <walyson.scarazzati@gmail.com>
2026-09-03 08:48:04 +0100

    72. Terminando a implementação de logistica e testando todo o fluxo

--- pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/service/AtualizacaoStatusPedidoService.java
+++ pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/service/AtualizacaoStatusPedidoService.java
@@ -17,8 +17,12 @@ public class AtualizacaoStatusPedidoService {
     public void atualizarStatus(Long codigo, StatusPedido status, String urlNotaFiscal, String rastreio){
         repository.findById(codigo).ifPresent(pedido -> {
             pedido.setStatus(status);
-            pedido.setUrlNotaFiscal(urlNotaFiscal);
-            pedido.setCodigoRastreio(rastreio);
+            if (urlNotaFiscal !=null){
+                pedido.setUrlNotaFiscal(urlNotaFiscal);
+            }
+            if(rastreio != null){
+                pedido.setCodigoRastreio(rastreio);
+            }
         });
     }
 }
